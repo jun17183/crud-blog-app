b095e97462367bf7bb67eaae6d9b0cdaebdadca9
jun17183 <jun17183@naver.com>
2023-10-22 21:11:39 +0900

    회원가입, 로그인, 로그아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
+import { app } from "firebaseApp";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import Router from "components/Router";
-import { Link } from "react-router-dom";
+import Loader from "components/Loader";
 
 function App() {
+  const auth = getAuth(app);
+  
+  // auth를 체크하기 전(initialize 전)에 loader를 띄워주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth의 currentUser가 있으면 true로 변경
+  const [isAuthenticated, setIsAuthentiated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setIsAuthentiated(true);
+      } else {
+        setIsAuthentiated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <>
-      <Router />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,33 @@
-import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 export default function Profile() {
+    const auth = getAuth(app);
+
+    const onSignOut = async () => {
+        try {
+            //const auth = getAuth(app);
+            await signOut(auth);
+            toast.success("로그아웃 되었습니다.");
+        } catch (err: any) {
+            toast.error(err?.code);
+            console.log(err);
+        }
+    }
+
     return (
         <div className="profile__box">
             <div className="flex__box-lg">
                 <div className="profile__image" />
                 <div>
-                    <div className="profile__email">jun17183@gmail.com</div>
-                    <div className="profile__name">준홍신</div>
+                    <div className="profile__email">{auth?.currentUser?.email}</div>
+                    <div className="profile__name">{auth?.currentUser?.displayName || "닉네임"}</div>
                 </div>
             </div>
-            <Link to="/" className="profile__logout">로그아웃</Link>
+            <div role="presentation" className="profile__logout" onClick={onSignOut}>
+                로그아웃
+            </div>
         </div>
     );
 }
