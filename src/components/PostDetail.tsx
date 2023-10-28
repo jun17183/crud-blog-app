f21d3d7fc3bc62757fdb5376c542c6c9e2ca5720
jun17183 <jun17183@naver.com>
2023-10-28 22:27:31 +0900

    조회, 수정

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,27 +1,72 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { PostProps } from "./PostList";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
+import { toast } from "react-toastify";
 
 export default function PostDetail() {
-    return (
-        <>
-            <div className="post__detail">
-                <div className="post__box">
-                    <div className="post__title">Post Title Sample Bla Bla</div>
-                    <div className="post__profile-box">
-                        <div className="post__profile" />
-                        <div className="post__author-name">jun17183</div>
-                        <div className="post__date">2023.10.15 Sun</div>
-                    </div>
-                    <div className="post__utils-box">
-                        <div className="post__delete">삭제</div>
-                        <div className="post__edit">
-                            <Link to={`/posts/edit/1`}>수정</Link>
-                        </div>
-                    </div>
-                    <div className="post__text">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                </div>
+  const [post, setPost] = useState<PostProps | null>(null);
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast.success("게시글을 삭제했습니다.");
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
+  return (
+    <>
+      <div className="post__detail">
+        {post ? (
+          <div className="post__box">
+            <div className="post__title">{post?.title}</div>
+            <div className="post__profile-box">
+              <div className="post__profile" />
+              <div className="post__author-name">{post?.email}</div>
+              <div className="post__date">{post?.createdAt}</div>
             </div>
-        </>
-    );
+            <div className="post__utils-box">
+              {post?.category && (
+                <div className="post__category">{post?.category}</div>
+              )}
+              <div
+                className="post__delete"
+                role="presentation"
+                onClick={handleDelete}
+              >
+                삭제
+              </div>
+              <div className="post__edit">
+                <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+              </div>
+            </div>
+            <div className="post__text post__text--pre-wrap">
+              {post?.content}
+            </div>
+          </div>
+        ) : (
+          <Loader />
+        )}
+      </div>
+    </>
+  );
 }
